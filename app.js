68ad929015755d001b17d4ea1dcbc79a27f48c45
Boat14101 <thanapon14101@gmail.com>
2022-10-22 13:01:32 +0700

    Add all files of project
    Hello

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) =>{
+    res.send('hello thanapon tt');
+})
+
+app.listen(port, ()=>{
+    debug("listening on port " + chalk.green(port));
+})
